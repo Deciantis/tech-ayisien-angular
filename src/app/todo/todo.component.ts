72b32b9828ad6cc492cfc31f8df8710cb64ad41c
Dee <test-email@gmail.com>
2021-05-23 14:35:14 -0400

    before 05232021

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-todo',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
+  //Tout bagay no vle sonje pou nou fe
+  todos = [];
+
+  faPlus = faPlus;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+//Pou nou mete na list nou
+  addTodo(bagaySa){
+    this.todos.push(bagaySa);
+    this.meteConsole();
+  }
 
+  meteConsole(){
+    this.todos.forEach((bagay) =>{
+          console.log(bagay);
+    });
+
+  }
 }

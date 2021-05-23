c0da75a400272267dec0d6808b5e5a9b2f08f31b
Dee <test-email@gmail.com>
2021-05-23 15:11:40 -0400

    05232021 AFTER

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
